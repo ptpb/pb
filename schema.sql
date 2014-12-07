7cfd89b6c998baa0fbc43a4fa491e6b7318987cf
Zack Buhman <zack@buhman.org>
2014-12-07 12:02:11 -0500

    url schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
+/*
+BEGIN PASTE SCHEMA
+*/
+
 DROP TABLE IF EXISTS paste;
-CREATE TABLE paste (  
+CREATE TABLE paste (
   id MEDIUMINT NOT NULL AUTO_INCREMENT,
   uuid BINARY(16) NOT NULL,
   digest BINARY(20) NOT NULL,
@@ -96,3 +100,67 @@ BEGIN
   WHERE id = p_id;
 END;
 @@
+
+/*
+END PASTE SCHEMA
+*/
+
+/*
+BEGIN URL SCHEMA
+*/
+
+DELIMITER ;
+
+DROP TABLE IF EXISTS url;
+CREATE TABLE url (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  digest BINARY(20) NOT NULL,
+  content BLOB NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY (digest)
+)
+ENGINE = InnoDB;
+
+DELIMITER @@
+
+DROP PROCEDURE IF EXISTS insert_url@@
+CREATE PROCEDURE insert_url (
+  p_content BLOB,
+  OUT p_id MEDIUMINT
+)
+BEGIN
+  START TRANSACTION;
+  INSERT url (digest, content)
+  VALUES (UNHEX(SHA1(p_content)), p_content);
+  SELECT last_insert_id() INTO p_id;
+  COMMIT;
+END;
+@@
+
+DROP PROCEDURE IF EXISTS get_url_digest@@
+CREATE PROCEDURE get_url_digest (
+  p_digest BINARY(20),
+  OUT p_id MEDIUMINT
+)
+BEGIN
+  SELECT id INTO p_id
+  FROM url
+  WHERE digest = p_digest;
+END;
+@@
+
+DROP PROCEDURE IF EXISTS get_url_content@@
+CREATE PROCEDURE get_url_content (
+  p_id MEDIUMINT,
+  OUT p_content BLOB
+)
+BEGIN
+  SELECT content INTO p_content
+  FROM url
+  WHERE id = p_id;
+END;
+@@
+
+/*
+END URL SCHEMA
+*/
