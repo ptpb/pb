33ffb242c553646bcb9b5a1db23d4d09055574e5
Zack Buhman <zack@buhman.org>
2014-12-20 11:09:12 -0500

    add testing skeleton
    gitignore the tox and distutils cruft; more better requirements.txt.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pb',
+    url='https://ptpb.pw',
+    license='GPLv3',
+    maintainer='Zack Buhman',
+    maintainer_email='zack@buhman.org',
+    packages=find_packages()
+)
