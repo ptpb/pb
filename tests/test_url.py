e26065bd988432d843242b40ddd114694bd7e657
Zack Buhman <zack@buhman.org>
2014-12-21 15:29:40 -0500

    add (complete) url tests

---
+++ tests/test_url.py
@@ -0,0 +1,52 @@
+from os import path
+from time import time
+from urllib import parse
+
+from flask import url_for
+
+from pb.pb import create_app
+from pb.util import b66_int
+
+shorturl = 'https://google.com'
+
+def test_url_post():
+    app = create_app()
+    
+    with app.test_request_context():
+        url = url_for('url.post')
+
+    rv = app.test_client().post(url)
+    assert rv.status_code == 400
+
+    f = lambda c: app.test_client().post(url, data=dict(
+        c = str(c)
+    ))
+    rv = f(shorturl)
+    assert rv.status_code == 200
+    
+    rv = f(time())
+    assert rv.status_code == 200    
+
+def test_url_get():
+    app = create_app()
+
+    with app.test_request_context():
+        url = url_for('url.post')
+
+    rv = app.test_client().post(url, data=dict(
+        c = str(time())
+    ))
+    location = rv.headers.get('Location') 
+    assert location
+
+    rv = app.test_client().get(location)
+    assert rv.status_code == 302
+
+    url_path = parse.urlsplit(location).path
+    id = b66_int(path.split(url_path)[-1])
+
+    with app.test_request_context():
+        url = url_for('url.get', b66=id+10)
+
+    rv = app.test_client().get(url)
+    assert rv.status_code == 404
