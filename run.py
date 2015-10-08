e8801f83b31010e767b1fe62efca75c0f4d2b2d0
Zack Buhman <zack@buhman.org>
2015-10-07 21:21:55 -0500

    shuffle run*.py around

--- run.py
+++ run.py
@@ -1,17 +1,1 @@
-#!/usr/bin/env python3
-"""
-    wsgi stub
-    ~~~~~~~~~
-
-    intended to be conveniently used by wsgi servers.
-
-    :copyright: Copyright (C) 2015 by the respective authors; see AUTHORS.
-    :license: GPLv3, see LICENSE for details.
-"""
-
-from pb.pb import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(host='::1', port=10002)
+pb/run.py
