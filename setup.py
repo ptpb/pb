294c128f96964060d7386ce0981b19ebe0e65386
Zack Buhman <zack@buhman.org>
2017-05-21 19:21:16 -0500

    setup: switch to pbr

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python3
+import setuptools
 
-from setuptools import setup, find_packages
-
-setup(
-    name='pb',
-    url='https://ptpb.pw',
-    license='GPLv3',
-    maintainer='Zack Buhman',
-    maintainer_email='zack@buhman.org',
-    include_package_data=True,
-    packages=find_packages(),
-    install_requires=open('requirements.txt').readlines(),
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True
 )
