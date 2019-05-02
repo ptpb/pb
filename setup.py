4b1cd3bbffa6a3631bb8361e7be600c0856deb54
Zack Buhman <zack@buhman.org>
2019-05-01 23:16:39 -0700

    setup.py: add install_requires

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import setuptools
 
+
+with open('requirements.txt') as f:
+    install_requires = list(f)
+
 setuptools.setup(
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
+    install_requires=install_requires,
 )
